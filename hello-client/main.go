29c4475fa76b298172d19675a88b43c6b118c456
hdheid <422537262@qq.com>
2024-01-11 15:02:03 +0800

    服务端和客户端的编写

--- hello-client/main.go
+++ hello-client/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	pb "gRPC_Study/hello-server/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	//通过grpc连接到server端，此处禁用安全传输，没有加密验证
+	conn, err := grpc.Dial("127.0.0.1:8888", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Printf("failed to connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	//创建客户端建立连接
+	client := pb.NewSayHelloClient(conn)
+
+	//执行rpc调用，在服务端实现具体逻辑并返回结果
+	client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "world!"})
+}
--- hello-server/main.go
+++ hello-server/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	pb "gRPC_Study/hello-server/proto"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type server struct {
+	pb.UnimplementedSayHelloServer
+}
+
+func (s server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{ResponseMsg: "hello" + req.RequestName}, nil
+}
+
+func main() {
+	//开启端口
+	listen, _ := net.Listen("tcp", ":8888")
+	//创建grpc服务
+	grpcServer := grpc.NewServer()
+	//注册自己编写的服务
+	pb.RegisterSayHelloServer(grpcServer, &server{})
+
+	//启动服务
+	err := grpcServer.Serve(listen)
+	if err != nil {
+		fmt.Println("failed to serve: ", err)
+		return
+	}
+}
